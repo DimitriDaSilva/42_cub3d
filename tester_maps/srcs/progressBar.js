cb926d89f23964363cfe4405ddd52568e5f49068
DimitriDS <dimitri.gomes.da.silva@gmail.com>
2021-02-23 19:11:34 +0000

    Deleted tester_map

--- tester_maps/srcs/progressBar.js
+++
@@ -1,7 +0,0 @@
-const cliProgress = require('cli-progress');
-
-const progressBar = new cliProgress.SingleBar({
-	format: '\n \033[33m {bar} \033[0m| {percentage}% | {value}/{total} Tests\n'
-}, cliProgress.Presets.shades_classic);
-
-module.exports = progressBar;
