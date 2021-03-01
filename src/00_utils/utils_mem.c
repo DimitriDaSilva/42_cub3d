286f3e28a0ce3800379b52fe023c4cf98ba40292
DimitriDS <dimitri.gomes.da.silva@gmail.com>
2021-03-01 18:32:36 +0000

    Continued to freeing the memory

--- src/00_utils/utils_mem.c
+++ src/00_utils/utils_mem.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils_mem.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dds <dda-silv@student.42lisboa.com>        +#+  +:+       +#+        */
+/*   By: dda-silv <dda-silv@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/01 17:31:06 by dda-silv          #+#    #+#             */
-/*   Updated: 2021/03/01 17:59:13 by dds              ###   ########.fr       */
+/*   Updated: 2021/03/01 18:32:12 by dda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,23 @@
 
 void	free_game(t_game *game)
 {
-	free_scene(&game->scene);
+	free_scene(&game->mlx.mlx_ptr, &game->scene);
 	free(game->rays.arr);
 }
 
-void	free_scene(t_scene *scene)
+void	free_scene(void *mlx_ptr, t_scene *scene)
 {
 	free(scene->no_tex.path);
+	free(scene->no_tex.img.data);
+	// mlx_destroy_image(mlx_ptr, scene->no_tex.img.img_ptr);
 	free(scene->so_tex.path);
+	free(scene->so_tex.img.data);
 	free(scene->we_tex.path);
+	free(scene->we_tex.img.data);
 	free(scene->ea_tex.path);
+	free(scene->ea_tex.img.data);
 	free(scene->sprite_tex.path);
+	free(scene->sprite_tex.img.data);
 	free_map(&scene->map);
 	free(scene->sprites);
 }
